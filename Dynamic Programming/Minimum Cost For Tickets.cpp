1e6764b5f01eb2dac206ccc1d8dfee1866458fb3
avinashvrm <66836647+avinashvrm@users.noreply.github.com>
2021-04-27 13:42:29 +0530

    Update Minimum Cost For Tickets.cpp

--- Dynamic Programming/Minimum Cost For Tickets.cpp
+++ Dynamic Programming/Minimum Cost For Tickets.cpp
@@ -1,25 +1,35 @@
-vector<int> dp;
-    int sub(vector<int> &day, vector<int> &cost, int si)    // subproblem
+class Solution {
+public:
+    vector<int> dp;
+    int sub(vector<int> &day, vector<int> &cost, int si)		// si denotes starting index
     {
+        //cout<<"| ";
         int n = day.size();
-        if(si>=n)   return 0;
-        
-        if(dp[si])  return dp[si];      // ie we aready knows the answer so no need to re calculate , return answer.
+        if(si>=n)   
+            return 0;
+        if(dp[si])  
+            return dp[si];
         
         int cost_day = cost[0] + sub(day , cost , si+1);
         
         int i;
-        for(i = si ; i<n and day[i]<day[si]+7 ; i++);
+        for(i = si ; i<n; i++)
+            if(day[i] >=day[si] + 7)
+                break;
         int cost_week = cost[1] + sub(day, cost, i);
         
-        for(i = si ; i<n and day[i]<day[si]+30 ; i++);
+        for(i = si ; i<n; i++)
+            if(day[i] >=day[si] + 30)
+                break;
         int cost_month = cost[2] + sub(day, cost, i);     
         
-        dp[si] = min({cost_day, cost_week , cost_month  });   // we store answer for future references 
-        
-        return dp[si];
-    } 
-    int mincostTickets(vector<int>& days, vector<int>& costs) {        
-        dp.resize(367);
-        return sub(days,costs,0);    
+        return dp[si] = min({cost_day, cost_week , cost_month  });
     }
+    
+    
+    int mincostTickets(vector<int>& days, vector<int>& costs) 
+    {      
+         dp.resize(367);
+        return sub(days,costs,0);
+    }
+};
