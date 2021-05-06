3079cd760165ae2cb98c69f0f60600286af9b88c
avinashvrm <66836647+avinashvrm@users.noreply.github.com>
2021-05-06 16:30:47 +0530

    LIS (LCS Approach + O(n) using set)

--- Dynamic Programming/Longest Increasing Subsequence.cpp
+++ Dynamic Programming/Longest Increasing Subsequence.cpp
@@ -33,3 +33,34 @@ public:
         return findLISLength(nums,n);
     }
 };
+
+//LCS Approch---First copy thr vector in another than remove all the repeating numbers than apply LCS
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums)
+    {
+        int n = nums.size();
+        
+        vector<int> vec(nums);
+        sort(vec.begin(), vec.end());
+        vec.erase( unique( vec.begin(), vec.end() ), vec.end() );//REMOVES ALL THE DUPLICATE NUMBERS
+        int m = vec.size();
+        int dp[n+1][m+1];
+        for(int i = 0; i<=n; i++)
+        {
+            for(int j = 0; j<=m; j++)
+            {
+                if(i==0 || j==0)
+                    dp[i][j] =0;
+                else if(nums[i-1]==vec[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+        
+    }
+};
+
