8049d9597b76c5c4c8d827485e44803215be2341
avinashvrm <66836647+avinashvrm@users.noreply.github.com>
2021-06-23 11:40:20 +0530

    Create Find the Most Competitive Subsequence.cpp

---
+++ Stack-Queue/Find the Most Competitive Subsequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public
+  
+  //Input: nums = [3,5,2,6], k = 2
+  //Output: [2,6]
+  //Explanation: Among the set of every possible subsequence: {[3,5], [3,2], [3,6], [5,2], [5,6], [2,6]}, [2,6] is the most competitive.
+  
+  //Input: nums = [2,4,3,3,5,4,9,6], k = 4
+  //Output: [2,3,3,4]
+    vector<int> mostCompetitive(vector<int>& nums, int k) 
+    {
+        int n = nums.size();
+        stack<int> st;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && st.top()>nums[i] && n-i-1 >= k-st.size())
+                st.pop();
+               
+            if(st.size() < k)
+                st.push(nums[i]);
+        }
+        vector<int> res;
+        while(!st.empty())
+        {
+            int t = st.top();st.pop();
+            res.push_back(t);
+        }
+        reverse(res.begin(),res.end());
+        return res;
+            
+    }
+};
