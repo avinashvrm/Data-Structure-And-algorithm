86fd7aebd537ff20c5dd628464da488743423f82
avinashvrm <66836647+avinashvrm@users.noreply.github.com>
2021-06-25 19:53:54 +0530

    Create Maximum Swap.cpp

---
+++ Random/Maximum Swap.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/maximum-swap/
+
+// Input: num = 2736
+// Output: 7236
+// Explanation: Swap the number 2 and the number 7.
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string n = to_string(num);
+        
+        // Keep in a map the last accurance of each digit:
+        unordered_map<int, int> last;
+        for (int i=0; i<n.size(); i++)
+            last[n[i] - '0'] = i;
+        
+        for (int i=0; i < n.size(); i++) 
+        {
+            for (int j = 9; j > n[i]-'0'; j--) 
+            {
+                
+                // If there's a larger digit later - swap:
+                if (last[j] > i) 
+                {
+                    swap(n[last[j]], n[i]);
+                    return stoi(n);
+                }
+            }
+        }
+        return stoi(n);
+    }
+};
