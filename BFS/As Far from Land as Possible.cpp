f0df17d23cc51209a0a23b143b812e7f747b2468
avinashvrm <66836647+avinashvrm@users.noreply.github.com>
2021-07-08 16:49:13 +0530

    Update As Far from Land as Possible.cpp

--- BFS/As Far from Land as Possible.cpp
+++ BFS/As Far from Land as Possible.cpp
@@ -1,46 +1,57 @@
-//see the hint section in leetcode
 class Solution {
 public:
-    int dx[4] = {0,0,1,-1}; 
-    int dy[4] = {1,-1,0,0};
-    bool visited[100][100] ={false};
-    int distance[100][100];
-    
-    int maxDistance(vector<vector<int>>& grid) 
+    int maxDistance(vector<vector<int>>& matrix) 
     {
-        int n = grid.size();
-        int ans = 0;
-        queue<pair<int,int>> q;
-        for(int i=0;i<n;i++) 
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        queue<pair<int,int> > q;
+        vector<vector<int> > dist(n, vector<int> (m, INT_MAX));
+
+        for(int i=0; i<n; i++)
         {
-            for(int j=0;j<n;j++) 
-            { 
-                if(grid[i][j])
-                {
-                    q.push({i,j});
-                    visited[i][j]=true;
-                    distance[i][j]=0;
-                }
-            }
-        }
-        // do a BFS traversal from all land('1') cells
-        while(!q.empty()) 
-        {
-            pair<int,int> v = q.front();
-            q.pop();
-            for(int i=0;i<4;i++)
+            for(int j=0; j<m; j++)
             {
-                int x = v.first + dx[i];
-                int y = v.second + dy[i];
-                if(x>=0 && y>=0 && x<n && y<n && !visited[x][y])
+                if(matrix[i][j] == 1)
                 {
-                    q.push({x,y});
-                    visited[x][y] = true;
-                    distance[x][y] = 1+distance[v.first][v.second];
-                    ans = max(ans,distance[x][y]);
+                    dist[i][j] = 0;
+                    q.push({i,j});
                 }
             }
         }
-        return ans==0 ? -1 : ans;
+        vector<int> dx = {-1,1,0,0};
+        vector<int> dy = {0,0,-1,1};
+        int max_dist = -1;
+        while(!q.empty())
+        {
+            int sz = q.size();
+
+            
+            for(int i=0; i<sz; i++)
+            {
+                int x = q.front().first;
+                int y = q.front().second;
+
+                q.pop();
+
+                for(int j=0; j<4; j++)
+                {
+                    int nx = x + dx[j];
+                    int ny = y + dy[j];
+
+                    if(nx >= 0 && ny >= 0 && nx < n && ny < m)
+                    {
+                        if(dist[nx][ny] > dist[x][y] + 1)
+                        {
+                            dist[nx][ny] = 1 + dist[x][y];
+                            q.push({nx,ny});
+                            max_dist = max(max_dist,dist[nx][ny]);
+                        }
+                    }
+                }
+            }
+        }
+
+        return max_dist;
     }
 };
