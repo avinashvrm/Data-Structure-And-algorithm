7b6be0397d54a8de21c788cbcb069ee810491387
avinashvrm <66836647+avinashvrm@users.noreply.github.com>
2021-07-19 21:57:11 +0530

    Using Both List(priority_queue) and Matrix(linear search)

--- Graph-algo/Prisms.cpp
+++ Graph-algo/Prisms.cpp
@@ -1,66 +1,127 @@
-int minKey(int key[], bool mstSet[], int V) {
-    // Initialize min value
-    int min = INT_MAX, min_index;
+//Using Ajacency List
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <utility>
 
-    for (int v = 0; v < V; v++)
-        if (mstSet[v] == false && key[v] < min) min = key[v], min_index = v;
+using namespace std;
+const int MAX = 1e4 + 5;
+typedef pair<long long, int> PII;
+bool marked[MAX];
+vector <PII> adj[MAX];
 
-    return min_index;
-}
-
-// A utility function to print the
-// constructed MST stored in parent[]
-int costofMST(int parent[], vector<vector<int>> &graph, int V) {
-    int cost = 0;
-    for (int i = 1; i < V; i++) cost += graph[i][parent[i]];
-    return cost;
-}
-
-// Function to construct and print MST for
-// a graph represented using adjacency
-// matrix representation, with V vertices.
-int spanningTree(int V, int E, vector<vector<int>> &graph) {
-    // Array to store constructed MST
-    int parent[V];
-
-    // Key values used to pick minimum weight edge in cut
-    int key[V];
-
-    // To represent set of vertices not yet included in MST
-    bool mstSet[V];
-
-    // Initialize all keys as INFINITE
-    for (int i = 0; i < V; i++) key[i] = INT_MAX, mstSet[i] = false;
-
-    // Always include first 1st vertex in MST.
-    // Make key 0 so that this vertex is picked as first vertex.
-    
-    key[0] = 0;
-    parent[0] = -1; // First node is always root of MST
-
-    // The MST will have V vertices
-    for (int count = 0; count < V - 1; count++) 
+long long prim(int x)
+{
+    priority_queue<PII, vector<PII>, greater<PII> > Q;
+    int y;
+    long long minimumCost = 0;
+    PII p;
+    Q.push(make_pair(0, x));
+    while(!Q.empty())
     {
-        // Pick the minimum key vertex from the
-        // set of vertices not yet included in MST
-        int u = minKey(key, mstSet, V);
-
-        // Add the picked vertex to the MST Set
-        mstSet[u] = true;
-
-        // Update key value and parent index of
-        // the adjacent vertices of the picked vertex.
-        // Consider only those vertices which are not
-        // yet included in MST
-        for (int v = 0; v < V; v++)
-
-            // graph[u][v] is non zero only for adjacent vertices of m
-            // mstSet[v] is false for vertices not yet included in MST
-            // Update the key only if graph[u][v] is smaller than key[v]
-            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
-                parent[v] = u, key[v] = graph[u][v];
+        // Select the edge with minimum weight
+        p = Q.top();
+        Q.pop();
+        x = p.second;
+        // Checking for cycle
+        if(marked[x] == true)
+            continue;
+        minimumCost += p.first;
+        marked[x] = true;
+        for(int i = 0;i < adj[x].size();++i)
+        {
+            y = adj[x][i].second;
+            if(marked[y] == false)
+                Q.push(adj[x][i]);
+        }
     }
+    return minimumCost;
+}
 
-    // print the constructed MST
-    return costofMST(parent, graph, V);
+int main()
+{
+    int nodes, edges, x, y;
+    long long weight, minimumCost;
+    cin >> nodes >> edges;
+    for(int i = 0;i < edges;++i)
+    {
+        cin >> x >> y >> weight;
+        adj[x].push_back(make_pair(weight, y));
+        adj[y].push_back(make_pair(weight, x));
+    }
+    // Selecting 1 as the starting node
+    minimumCost = prim(1);
+    cout << minimumCost << endl;
+    return 0;
+}
+// Using Ajacency Matrix
+#include<bits/stdc++.h>
+using namespace std;
+
+#define V 6		//No of vertices
+
+int selectMinVertex(vector<int>& value,vector<bool>& setMST)
+{
+	int minimum = INT_MAX;
+	int vertex;
+	for(int i=0;i<V;++i)
+	{
+		if(setMST[i]==false && value[i]<minimum)
+		{
+			vertex = i;
+			minimum = value[i];
+		}
+	}
+	return vertex;
+}
+
+void findMST(int graph[V][V])
+{
+	int parent[V];		//Stores MST
+	vector<int> value(V,INT_MAX);	//Used for edge relaxation
+	vector<bool> setMST(V,false);	//TRUE->Vertex is included in MST
+
+	//Assuming start point as Node-0
+	parent[0] = -1;	//Start node has no parent
+	value[0] = 0;	//start node has value=0 to get picked 1st
+
+	//Form MST with (V-1) edges
+	for(int i=0;i<V-1;++i)
+	{
+		//Select best Vertex by applying greedy method
+		int U = selectMinVertex(value,setMST);
+		setMST[U] = true;	//Include new Vertex in MST
+
+		//Relax adjacent vertices (not yet included in MST)
+		for(int j=0;j<V;++j)
+		{
+			/* 3 constraints to relax:-
+			      1.Edge is present from U to j.
+			      2.Vertex j is not included in MST
+			      3.Edge weight is smaller than current edge weight
+			*/
+			if(graph[U][j]!=0 && setMST[j]==false && graph[U][j]<value[j])
+			{
+				value[j] = graph[U][j];
+				parent[j] = U;
+			}
+		}
+	}
+	//Print MST
+	for(int i=1;i<V;++i)
+		cout<<"U->V: "<<parent[i]<<"->"<<i<<"  wt = "<<graph[parent[i]][i]<<"\n";
+}
+
+int main()
+{
+	int graph[V][V] = { {0, 4, 6, 0, 0, 0},
+						{4, 0, 6, 3, 4, 0},
+						{6, 6, 0, 1, 8, 0},
+						{0, 3, 1, 0, 2, 3},
+						{0, 4, 8, 2, 0, 7},
+						{0, 0, 0, 3, 7, 0} };
+
+	findMST(graph);	
+	return 0;
 }
