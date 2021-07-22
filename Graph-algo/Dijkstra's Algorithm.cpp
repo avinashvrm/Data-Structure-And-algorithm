6035e7bd83c014cf34bf18d5740bc25d875e962b
avinashvrm <66836647+avinashvrm@users.noreply.github.com>
2021-07-22 17:00:18 +0530

    Update Dijkstra's Algorithm.cpp

--- Graph-algo/Dijkstra's Algorithm.cpp
+++ Graph-algo/Dijkstra's Algorithm.cpp
@@ -30,3 +30,37 @@ void dijkstra()
         }
     }
 }
+// using min heap
+void dijkstra()
+{
+			priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+			vector<int>dist(N+1, INT_MAX);
+			
+			pq.push({0, K});
+			dist[K] = 0;
+			
+			unordered_map<int, vector<pair<int, int>>>hm;
+			for (int i = 0; i < times.size(); i++)
+				hm[times[i][0]].push_back({times[i][1], times[i][2]});
+			
+			while (!pq.empty()) 
+      {
+				pair<int, int>p = pq.top();
+				pq.pop();
+				
+				int u = p.second;
+				for (auto it : hm[u]) 
+        {
+					int v = it.first;
+					int w = it.second;
+					
+					if (dist[v] > dist[u] + w)
+          {
+						dist[v] = dist[u] + w;
+						pq.push({dist[v], v});
+					}
+				}
+			}
+			
+		}
+	};
