bbe5b4a92fe57a51b760dcaa542b7f2f53325dec
avinashvrm <66836647+avinashvrm@users.noreply.github.com>
2021-08-05 11:46:10 +0530

    Update Best Time to Buy and Sell Stock III.cpp

--- Dynamic Programming/Best Time to Buy and Sell Stock III.cpp
+++ Dynamic Programming/Best Time to Buy and Sell Stock III.cpp
@@ -1,3 +1,4 @@
+//iss post m sara diya h, apne code se acha usse smjh aega!!!
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/discuss/900051/fully-explained-all-buy-and-sell-problems-c-recursive-memoization-minor-difference
 //Problem solutions
 class Solution {
@@ -28,5 +29,32 @@ public:
             ans = max(ans,v1[i] + v2[i]);
         return ans;
         
+    }
+};
+// O(N*K)
+class Solution {
+public:
+    int find(vector<int> &prices,int ind,bool buy,int c,vector<vector<vector<int>>> &memo)
+    {   
+        //if buy =1 means we have to buy now
+        //else we have to sell now
+        if(ind>=prices.size()||c>=2) return 0; //counter
+        if(memo[ind][buy][c]!=-1) return memo[ind][buy][c];
+        if(buy) //now we can either buy prices[i] or we can skip it and try next to buy
+        {
+            return memo[ind][buy][c]=max(-prices[ind]+find(prices,ind+1,!buy,c,memo),find(prices,ind+1,buy,c,memo));
+        }
+        else  //now we can either sell prices[i] or we can skip it and try next to sell
+        {
+            return memo[ind][buy][c]=max(prices[ind]+find(prices,ind+1,!buy,c+1,memo),find(prices,ind+1,buy,c,memo));
+        }
+        
+    }
+    int maxProfit(vector<int>& prices) {
+        //here we can do maximum two transaction
+        //Use 3-D vector because here three states i,k,buy/sell
+        vector<vector<vector<int>>> memo(prices.size(),vector<vector<int>>(2,vector<int>(2,-1)));
+          
+       return find(prices,0,1,0,memo); 
     }
 };
