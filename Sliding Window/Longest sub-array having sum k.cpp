e51e031af6cb8cb1dc690c31112c11dea52b415f
avinashvrm <66836647+avinashvrm@users.noreply.github.com>
2021-08-20 16:28:32 +0530

    Create Longest sub-array having sum k.cpp

---
+++ Sliding Window/Longest sub-array having sum k.cpp
@@ -0,0 +1,61 @@
+/*Given an array arr[] of size n containing integers. The problem is to find the length of the longest sub-array having sum equal to the given value k.
+  Input : arr[] = { 10, 5, 2, 7, 1, 9 }, 
+            k = 15
+  Output : 4
+  The sub-array is {5, 2, 7, 1}.
+
+  Input : arr[] = {-5, 8, -14, 2, 4, 12},
+              k = -5
+  Output : 5
+*/
+    
+// C++ implementation to find the length
+// of longest subarray having sum k
+#include <bits/stdc++.h>
+using namespace std;
+
+// function to find the length of longest
+// subarray having sum k
+int lenOfLongSubarr(int arr[],int n,int k)
+{
+	// unordered_map 'um' implemented
+	// as hash table
+	unordered_map<int, int> um;
+	int sum = 0, maxLen = 0;
+
+	// traverse the given array
+	for (int i = 0; i < n; i++) {
+
+		// accumulate sum
+		sum += arr[i];
+
+		// when subarray starts from index '0'
+		if (sum == k)
+			maxLen = i + 1;
+
+		// make an entry for 'sum' if it is
+		// not present in 'um'
+		if (um.find(sum) == um.end())
+			um[sum] = i;
+
+		// check if 'sum-k' is present in 'um'
+		// or not
+		if (um.find(sum - k) != um.end()) 
+    {	// update maxLength
+			if (maxLen < (i - um[sum - k]))
+				maxLen = i - um[sum - k];
+		}
+	}
+  return maxLen;
+}
+
+// Driver Code
+int main()
+{
+	int arr[] = {10, 5, 2, 7, 1, 9};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int k = 15;
+	cout << "Length = "
+		<< lenOfLongSubarr(arr, n, k);
+	return 0;
+}
