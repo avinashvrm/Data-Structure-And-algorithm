767c504413a59bddad8763844d334868a8a972fa
avinashvrm <66836647+avinashvrm@users.noreply.github.com>
2021-09-01 19:34:15 +0530

    Create Minimum Number of Work Sessions to Finish the Tasks.cpp

---
+++ Dynamic Programming/Minimum Number of Work Sessions to Finish the Tasks.cpp
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/minimum-number-of-work-sessions-to-finish-the-tasks/
+// Go to the link and prectice once, dp with bit masking question, you were clueless on the first go!,so plz revise
+
+class Solution {
+public:
+    long long dp[1<<15][180];
+    int find(vector<int>&nums,int mask,int sum,int se,int n)
+    {
+        if(mask==(1<<n)-1)
+        {
+            return 1;
+        }
+        int ans=INT_MAX;
+        if(dp[mask][sum]!=-1)
+        {
+            return dp[mask][sum];
+        }
+        for(int i=0;i<n;i++)
+        {
+            if((mask&(1<<i))==0)
+            {
+                if(sum+nums[i]>se)
+                {
+                    ans=min(ans,1+find(nums,mask|(1<<i),nums[i],se,n));
+                }
+                else
+                {
+                    ans=min(ans,find(nums,mask|(1<<i),sum+nums[i],se,n));
+                }
+            }
+        }
+        return dp[mask][sum]=ans;
+    }
+    int minSessions(vector<int>&nums, int se)
+    {
+        memset(dp,-1,sizeof(dp));
+        return find(nums,0,0,se,nums.size());
+    }
+};
