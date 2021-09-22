2f3d7d041d7cc4cdb67fc60bb91ece663c2e5e26
Avinash Verma <66836647+avinashvrm@users.noreply.github.com>
2021-09-22 19:59:43 +0530

    Update 146. LRU Cache.cpp

--- solutions/146. LRU Cache.cpp
+++ solutions/146. LRU Cache.cpp
@@ -48,3 +48,114 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+
+//USING DLL
+class Node
+{
+public:
+    int key,val;
+    Node* left;
+    Node* right;
+    Node(int key,int val)
+    {
+        this->key = key;
+        this->val = val;
+        this->left = NULL;
+        this->right= NULL;
+    }
+};
+class LRUCache {
+public:
+    Node *head,*tail;
+    unordered_map<int,Node*> mp;
+    int n,cap;
+    LRUCache(int capacity) 
+    {
+        this->cap=capacity;
+        this->n=0;
+        this->head=NULL;
+        this->tail=NULL;
+    }
+    void del(Node *x)
+    {
+        if(x->left!=NULL)
+        {
+            x->left->right = x->right;
+            
+        }
+        else
+            head = x->right;
+        
+        if(x->right!=NULL)
+        {
+            x->right->left = x->left;
+        }
+        else
+            tail = x->left;
+        // delete(x);
+        x->left=NULL;
+        x->right=NULL;
+    }
+    void insert(Node *x)
+    {
+        if(head==NULL)
+        {
+            head = x;
+            tail = x;
+            return;
+        }
+        tail->right = x;
+        x->left = tail;
+        tail  = x;
+    }
+     /*
+    Logic for get function:
+    -> if key not found return -1
+    ->if found then return val
+    */
+    int get(int key) 
+    {
+        if(mp.find(key)!=mp.end())
+        {
+            Node *x = mp[key];
+            del(x);
+            insert(x);
+            return x->val;
+        }
+        else
+            return -1;
+    }
+    /*Logic for Put function:
+    ->if key already present thenchange the value of the key insert it at last
+    ->if key not present
+        Case1:if size== capacity then first value removed in LRU Cache
+        Case2:if size!=capacity value inserted at last  
+    */
+    void put(int key, int value) 
+    {
+         
+        if(mp.find(key)!=mp.end())
+        {
+            mp[key]->val = value;
+            del(mp[key]);
+            insert(mp[key]);//becoz of here we are not performing delete() at line 43
+        }
+        else
+        {
+            Node *newnode = new Node(key,value);
+            mp[key] = newnode;
+            if(n==cap)
+            {
+                mp.erase(head->key);
+                del(head);
+                insert(newnode);
+            }
+            else
+            {
+                n++;
+                insert(newnode);
+            }
+        }
+        
+    }
+};
