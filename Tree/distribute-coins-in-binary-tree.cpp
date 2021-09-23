91a04346283845294089bc6906cf2d3b42c02ae8
Avinash Verma <66836647+avinashvrm@users.noreply.github.com>
2021-09-23 13:28:10 +0530

    Create distribute-coins-in-binary-tree.cpp

---
+++ Tree/distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/distribute-coins-in-binary-tree/
+
+
+
+class Solution {
+public:
+    int solve(TreeNode *root,int &moves)
+    {
+        if(!root)
+            return 0;
+        int l = solve(root->left,moves);
+        int r = solve(root->right,moves);
+        
+        moves += abs(l) + abs(r);
+        return l + r + root->val -1;
+    }
+    int distributeCoins(TreeNode* root) 
+    {
+        int moves = 0;
+        solve(root,moves);
+        return moves;
+    }
+};
