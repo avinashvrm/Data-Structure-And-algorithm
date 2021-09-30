cdf353a94ba9cf101808fe98195b1c4e9ca18ac0
Avinash Verma <66836647+avinashvrm@users.noreply.github.com>
2021-09-30 14:49:33 +0530

    Update Fenwick-Tree Implementation.cpp

--- Segment Tree/Fenwick-Tree Implementation.cpp
+++ Segment Tree/Fenwick-Tree Implementation.cpp
@@ -14,78 +14,62 @@ void s_g_c()
     freopen("output.txt", "w", stdout);
 #endif
 }
-// Implementation of a Binary Indexed Tree (Fennwick Tree)
-class BIT {
-private:
-	vector<int> fen;
-public:
-	BIT(vector<int> &arr) 
-	{
-		// Initialize BIT with arr in O(n)
-		fen.resize(arr.size() + 1, 0);
-		for (int idx = 0; idx < arr.size(); idx++) 
-		{
-			fen[idx + 1] = arr[idx];
-		}
-
-		for (int idx = 1; idx < fen.size(); idx++) 
-		{
-			int idx2 = idx + (idx & -idx);
-			if (idx2 < fen.size()) 
-			{
-				fen[idx2] += fen[idx];
-			}
-		}
-		for (int idx = 1; idx < fen.size(); idx++) 
-		{
-			cout<<fen[idx]<<"  ";
-		}
-		cout<<endl<<endl;
-	}
-
-	int prefix_query(int idx) const 
-	{
-		// Computes prefix sum of up to fenthe element at index idx
-		int result = 0;
-		++idx; 
-		for (;idx > 0; idx -= idx & -idx) 
-		{
-			result += fen[idx];
-		}
-		return result;
-	}
-
-	int range_query(int from_idx, int to_idx) const 
-	{
-		// Computes the range sum between two indices (both inclusive)
-        if (from_idx == 0)
-            return prefix_query(to_idx);
-        else
-            return prefix_query(to_idx) - prefix_query(from_idx - 1);
-	}
-
-	void update(int idx, int add) 
-	{
-		// Add a value to the element at index idx
-		++idx; 
-		for (;idx < fen.size(); idx += idx & -idx) 
-		{
-			fen[idx] += add;
-		}
-	}
-};
-int main()
+void updateTree(vector<int>& tree,int n,int i,int val)
 {
-	s_g_c();
+    i++;
+    while(i<=n)
+    {
+        tree[i]+=val;
+        i+=i&(-i);
+    }
+}
+int getSum(vector<int> tree,int n,int i)
+{
+    i++;
+    int sum=0;
+    while(i>0)
+    {
+        sum+=tree[i];
+        i-=i&(-i);
+    }
+    return sum;
+}
+void createTree(vector<int> v,int n,vector<int>& tree)
+{
+    vector<int> pre(n+1);
+    pre[1] = v[0];
 
-	vector<int> array{ 1, 7, 3, 0, 5, 8, 3, 2, 6, 2, 1, 1, 4, 5 };
-  int length = array.size();
-	BIT bit(array);
-	int res1 = bit.range_query(0,4);
-	cout<<res1<<endl;
-	bit.update(0, 1);
-	int res = bit.range_query(0,4);
-	cout<<res;
-	return 0;
+    for(int i=2;i<=n;i++)
+        pre[i] = pre[i-1] + v[i-1];
+
+    for(int i=1;i<=n;i++)
+    {
+        int idash = i-(i&-i);
+        tree[i] = pre[i] - pre[idash];
+    }
+    // for(int i=0;i<n;i++)
+    // {
+    //     updateTree(tree,n,i,v[i]);//this is nlog(n)
+    // }
+}
+int main(){
+    s_g_c();
+    int n;
+    cin>>n;
+    vector<int> v(n,0);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    vector<int> tree(n+1,0);
+    createTree(v,n,tree);
+    for(int i=1;i<=n;i++)
+        cout<<tree[i]<<" ";
+    cout<<endl;
+    cout<<getSum(tree,n,3)<<endl;
+    
+    //update
+    v[3]=6;
+    updateTree(tree,n,3);//diff b/w new and old value
+    cout<<getSum(tree,n,4);
 
 }
